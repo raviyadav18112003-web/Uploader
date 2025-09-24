3fe75f21c207663a611943e4252bfaedaa8c6ca6
raviyadav18112003-web <raviyadav18112003@gmail.com>
2025-09-24 10:36:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -44,3 +44,8 @@ def hello_world():
 
 if __name__ == "__main__":
     app.run()
+
+if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
